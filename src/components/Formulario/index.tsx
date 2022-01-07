d33be52970ff308c73d87cf779b4647de071fd89
IzabelaGonzaga <izabela.silva@ambevtech.com.br>
2022-01-07 19:32:18 -0300

    update curso alura

---
+++ src/components/Formulario/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Botao from '../Botao';
+import styles from './Formulario.module.scss'
+
+class Formulario extends React.Component{
+    render(){
+        return(
+            <form className={styles.novaTarefa}>
+                <div className={styles.inputContainer}>
+                    <label> Adicione um novo estudo </label>
+                    <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar?" required />
+                </div>
+                <div className={styles.inputContainer}>
+                    <label htmlFor="tempo"> Tempo </label>
+                    <input type="time" step="1" name="tempo" id="tempo" min="00:00:00" max="01:30:00" />
+                </div>
+                <Botao />
+            </form>
+        )
+    }
+}
+
+export default Formulario;
---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from './Lista.module.scss'
+
+export default function Lista(){
+    const tarefas = [{
+        tarefa: 'React',
+        tempo: '02:00:00'
+    }];
+
+    return(
+        <aside className={styles.listaTarefas}>
+            <h2> Estudos do dia </h2>
+            <ul>
+                {tarefas.map((tarefa, index) => <li key={index} className={styles.item}>
+                                            <h3> {tarefa.tarefa} </h3>
+                                            <span> {tarefa.tempo} </span>
+                                        </li>
+                )} 
+            </ul>
+        </aside>
+    )
+}
